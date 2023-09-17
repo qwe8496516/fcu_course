64d24ce6ac873b59a4681e1a010396574f043db6
jeffYang1224 <92380307+qwe8496516@users.noreply.github.com>
2023-09-17 12:10:25 +0800

    搶課

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import logging
 import ddddocr
 import json
@@ -47,47 +48,63 @@ def Login(browser):
 
 # 開始搶課
 def rob(browser,click_interval):    
-    # 取得課程代號列表
-    Course_List = get_current_course()
-
     # 檢查課程是否有餘額
-    while len(Course_List):
-        # 餘額數量
-        remainValue=0
-        # ------------------------------------------------------------------------------------------ 未知
-        try:
-            browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_Label3').click()
-            browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_tbSubID').send_keys(Course_List[len(Course_List)-1])
-            browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_gvToAdd"]/tbody/tr[2]/td[8]/input').click()
+    
+    # 餘額數量
+    remainValue=0
+    # ------------------------------------------------------------------------------------------ 未知
+    try:
+        browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_Label3').click()
+
+        while(True):
             
+            # 取得課程代號列表
+            Course_List = get_current_course()
+            print(Course_List,len(Course_List))
+
+            if (Course_List[0] == ""):
+                break
+
             # 控制點擊時長
-            sleep(click_interval)
+            browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_tbSubID').send_keys(Course_List[0])
+            
+            browser.find_element_by_css_selector("#ctl00_MainContent_TabContainer1_tabSelected_gvToAdd tr:nth-child(2) td:first-child input").click()
 
             # 從資訊中取得目前餘額 / 開放名額
-            alert = browser.switch_to_alert()
-            alertInfo = alert.text
-            remainValue = int(alertInfo[10:13].strip())
-            originValue = int(alertInfo[14:18].strip())
-            alert.accept()
-            print('目前餘額 : ', remainValue)
-            print('開放名額 : ', originValue)
-        except:
-            print('',end='')
-            
-        # 執行選課功能  
-        if(remainValue > 0):
-            try:
-                # 點擊該課程加選按鈕
-                browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_gvToAdd/tbody/tr[2]/td[1]/input').click()
-                if browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock/span').text.find('加選成功') != -1:
-                    print(browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock/span').text)
-                    Course_List.remove(Course_List[len(Course_List)-1])
-                else:
-                    print(browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock/span').text)
-            except:
-                print('',end='')      
+            # alert = browser.switch_to_alert()
+            # alertInfo = alert.text
+            # remainValue = int(alertInfo[10:13].strip())
+            # originValue = int(alertInfo[14:18].strip())
+            # alert.accept()
+            # print('目前餘額 : ', remainValue)
+            # print('開放名額 : ', originValue)
+
+
+            if browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock').find_element_by_tag_name('span').text == '加選成功':
+                print(browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock').find_element_by_tag_name('span').text)
+                remove_current_course()
+            else:
+                print(browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock').find_element_by_tag_name('span').text)
+
+
+            sleep(click_interval)
+    except:
+        print('',end='')
         
-        browser.get(browser.current_url)
+    # 執行選課功能  
+    # if(remainValue > 0):
+    #     try:
+    #         # 點擊該課程加選按鈕
+    #         browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_gvToAdd/tbody/tr[2]/td[1]/input').click()
+    #         if browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock/span').text.find('加選成功') != -1:
+    #             print(browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock/span').text)
+    #             Course_List.remove(Course_List[len(Course_List)-1])
+    #         else:
+    #             print(browser.find_element_by_id('ctl00_MainContent_TabContainer1_tabSelected_lblMsgBlock/span').text)
+    #     except:
+    #         print('',end='')      
+    
+    # browser.get(browser.current_url)
 
 
 # 透過 webdriver 開啟瀏覽器
@@ -145,3 +162,21 @@ def get_current_course():
         data = json.load(file)
         course_list = [data["course_ID1"],data["course_ID2"],data["course_ID3"],data["course_ID4"]]
         return course_list
+    
+
+
+# 清除已加選的課程代碼
+def remove_current_course():
+    with open("./data/course.json", 'r+') as file:
+        data = json.load(file)
+
+        # 將 course_ID4 的值設為空
+        data["course_ID4"] = ""
+
+        # 依次將值往後移
+        data["course_ID1"] = data["course_ID2"]
+        data["course_ID2"] = data["course_ID3"]
+        data["course_ID3"] = data["course_ID4"]
+        # 寫入修改後的 JSON 資料到檔案
+        with open("./data/course.json", 'w') as file:
+            json.dump(data, file, indent=4)
